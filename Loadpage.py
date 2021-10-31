c19d54407dffbbd4207bcc298281677f3636e871
A. Santhosh Kumar <asanthoshkumar01@gmail.com>
2021-10-31 21:07:37 +0530

    scarypage

---
+++ Loadpage.py
@@ -0,0 +1,15 @@
+import pygame
+from time import sleep
+pygame.init()
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('piano.mp3')
+pygame.mixer.music.play()
+sleep(5)
+pygame.mixer.music.load('scary.mp3')
+pygame.mixer.music.play()
+sleep(4)
+image = pygame.image.load('nun.gif')
+window.blit(image, (0,0))
+pygame.display.update()
+sleep(4)
